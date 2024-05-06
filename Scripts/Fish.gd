ec3e1715fe9896ece4ca1e7db2d1d78ff680441e
Jermel Lapaz <149228096+jrmellpaz@users.noreply.github.com>
2024-05-06 12:36:01 +0800

    Add files via upload

---
+++ Scripts/Fish.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+var movement_speed = 4
+var movement_time = 1
+
+var min_distance = 100
+var max_distance = 200
+
+var min_position = 20
+var max_position = 290
+
+func _ready():
+	plan_move()
+	
+func plan_move():
+	var target = randf_range(min_position, max_position)
+	
+	while(abs(self.position.x - target) < min_distance or abs(self.position.x - target) > max_distance):
+		target = randf_range(min_position, max_position)
+		
+	move(Vector2(target, self.position.y))
+
+	
+func move(target):
+	var tween = create_tween().bind_node(self)
+	tween.tween_property(self, "position", target, movement_time) # not sure about movement_time, but it works
+	tween.play()
+	
+	$MoveTimer.set_wait_time(movement_time)
+	$MoveTimer.start()
+
+func destroy():
+	get_parent().remove_child(self)
+	queue_free()
+	
+func timeout():
+	plan_move()
