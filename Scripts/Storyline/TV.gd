a7d7812b943c934a69a9b1335b56bc1b1511bbd9
Az <arbusinos@up.edu.ph>
2024-06-07 13:50:31 +0800

    [az] clutch

--- Scripts/Storyline/TV.gd
+++ Scripts/Storyline/TV.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	$AnimatedSprite2D.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
